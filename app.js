46a980d2fd093d21e84b4b91231bb2b317d5e45d
Sara Isa <saraisamo123@gmail.com>
2021-11-03 03:06:15 +0300

    first heroku commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const path = require("path");
+
 require("dotenv").config({ path: "./config.env" });
 require("./server");
 
